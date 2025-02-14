bba3362b70951dc0d27395bc8e2b2067a55140d8
IgnacioSY00 <alumno.712033@ies-azarquiel.es>
2025-02-14 13:08:19 +0100

    6

--- src/main/java/Clases/pruebita.java
+++ src/main/java/Clases/pruebita.java
@@ -1,8 +1,13 @@
 package Clases;
 import Clases.Factura.*;
+import Clases.Coche.*;
 public class pruebita {
 
     public static void main(String[] args) {
-
+    Factura f = new Factura();
+    Coche c = new Coche();
+    c.
+    f.
+    System.out.println(Factura.getCuantas());
     }
 }
