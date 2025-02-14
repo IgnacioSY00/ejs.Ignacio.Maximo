be3ab2d92282932cdf11a2dc57e7b338f241132f
IgnacioSY00 <alumno.712033@ies-azarquiel.es>
2025-02-14 12:50:17 +0100

    4

---
+++ src/main/java/Clases/UsoCoche.java
@@ -0,0 +1,12 @@
+package Clases;
+
+public class UsoCoche {
+    public static void main(String[] args) {
+        Coche c=new Coche();
+        System.out.println("c.getNumRuedas() = " + c.getNumRuedas());
+        System.out.println("c.getMatricula() = " + c.getMatricula());
+        System.out.println("c = " + c);
+    }
+
+
+}
